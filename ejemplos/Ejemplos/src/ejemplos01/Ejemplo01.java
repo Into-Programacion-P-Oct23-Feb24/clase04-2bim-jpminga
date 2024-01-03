8a5d1601fed7a528a4a50d1f53cb003cc9648d2f
jpminga <jpminga2@utpl.edu.ec>
2024-01-03 16:54:57 -0500

    cambios

--- ejemplos/Ejemplos/src/ejemplos01/Ejemplo01.java
+++ ejemplos/Ejemplos/src/ejemplos01/Ejemplo01.java
@@ -5,8 +5,6 @@
  */
 package ejemplos01;
 
-//import java.util.Locale;
-
 /**
  *
  * @author reroes
@@ -18,36 +16,48 @@ public class Ejemplo01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        
+
 //        String miCiudad = obtenerCiudadMayuscula("Loja");
 //        System.out.printf("%s\n", miCiudad);
-        obtenerMultiplicacion(7,50);
-        String miMensaje;
-        System.out.printf("%s\n",miMensaje);
+         obtenerMultiplicacion(7,50);
+        //aqui no retornamos la información debido a que es un procedimiento
+
+        String miMensaje = obtenerMultiplicacion(7, 50);
+        // Aqui obtenerMultiplicacion es una funcion y luego
+        // retorna a un valor en este caso de tipo cadena 
+        System.out.printf("%s\n", miMensaje);
+
     }
-    
-    
-    public static String obtenerCiudadMayuscula(String m){
+
+    public static String obtenerMultiplicacion(int tabla, int limite) {
+        int m = 0;
+        String cadena = "";
+        for (int i = 1; i <= limite; i++) {
+            m = tabla * i;
+            cadena = String.format("%s%d + %d = %d\n", cadena,
+                    tabla, i, m);
+
+        }
+        return cadena;
+    }
+
+    public static String obtenerCiudadMayuscula(String m) {
         String m2 = m.toUpperCase();
         return m2;
     }
-    
-    public static void obtenerMultiplicacion(int tabla, int limite){
-        String cadenaFinal = "";
-        int resultado = 0;
-        for (int i = 1; i <= limite; i++) {
-            resultado = tabla * i;
-            cadenaFinal = String.format("%s%d * %d = %d\n"
-                    ,cadenaFinal,tabla,i,resultado);
-        }
-//        System.out.printf("%s\n",cadenaFinal);
-        return cadenaFinal;
-    }
-    
-    public static String obtenerNombre(){
+
+//    public static void obtenerMultiplicacion(int tabla, int limite){
+//        String cadenaFinal = "";
+//        int resultado = 0;
+//        for (int i = 1; i <= limite; i++) {
+//            resultado = tabla * i;
+//            cadenaFinal = String.format("%s%d * %d = %d\n"
+//                    ,cadenaFinal,tabla,i,resultado);
+//        }
+//        System.out.printf("%s\n",cadenaFinal);  
+//    }
+    public static String obtenerNombre() {
         return "Luis";
     }
-    
-    
+
 }
--- ejemplos/Ejemplos/src/ejemplos02/Ejemplo041.java
+++ ejemplos/Ejemplos/src/ejemplos02/Ejemplo041.java
@@ -13,16 +13,40 @@ public class Ejemplo041 {
 
     public static void main(String[] args) {
         // 
-        obtenerMultiplicacion(primerValor, segundoValor); 
+        int[][] arregloN1 = {{1, 2, 3}, {3, 2, 4}, {2, 6, 2}};
+        int[][] arregloN2 = {{1, 2, 3}, {2, 2, 2}, {3, 1, 2}};
+        int[][] arregloFinal = new int[3][3];
+        String acumulador1 = "";
+        String acumulador2 = "";
+        String acumuladorFinal = "";
         
+        for (int f = 0; f < arregloN1.length; f++) {
+            for (int c = 0; c < arregloN1[f].length; c++) {
+                arregloFinal[f][c] = obtenerMultiplicacion(arregloN1[f][c],
+                         arregloN2[f][c]);
+            }
+
+        }
+
+        for (int f = 0; f < arregloN1.length; f++) {
+            for (int c = 0; c < arregloN1[f].length; c++) {
+              acumulador1 = String.format("%s%d  ",acumulador1, arregloN1[f][c]);
+              acumulador2 = String.format("%s%d  ",acumulador2, arregloN2[f][c]);
+              acumuladorFinal = String.format("%s%d  ",acumuladorFinal, arregloFinal[f][c]);
+            }
+            acumulador1 = String.format("%s\n ",acumulador1);
+            acumulador2 = String.format("%s\n ",acumulador2);
+            acumuladorFinal = String.format("%s\n ",acumuladorFinal);
+        }
+        
+
     }
-        
-    public static void obtenerSuma(int a, int b){
+
+    public static int obtenerMultiplicacion(int a, int b) {
         int suma;
-        suma = a + b;
-        System.out.printf("El valor de la suma es: %d\n", suma);
-        
+        suma = a * b;
+
+        return suma;
     }
-    
-    
+
 }
